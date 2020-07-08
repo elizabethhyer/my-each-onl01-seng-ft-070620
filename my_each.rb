b904e43fe02a06a033b578b24f5f7e4dab56aad4
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-08 20:18:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,24 +2,15 @@ collection = []
 
 def my_each(words) do |word| # put argument(s) here
   # code here
+  if block_given?
   x = 0 
   
-  while x 
-end
-
-
-
-def hello_t(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
+  while x < words.length 
+  yield(word[x])
+  x = x + 1 
+  end 
+    words
   else
-    puts "Hey! No block was given!"
-  end
-end
+    "Where's my block?"
+  end 
+end 
