f4c96edc1dbbe4c6cdaed44afa81f2137c074365
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-08 20:34:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -15,18 +15,3 @@ def my_each(words) # put argument(s) here
   end 
 end 
 
-An error occurred while loading ./spec/my_each_spec.rb.
-Failure/Error: require_relative '../my_each.rb'
-
-SyntaxError:
-  /home/hyer/my-each-onl01-seng-ft-070620/my_each.rb:3: syntax error, unexpected do
-  def my_each(words) do |word| # put argument(s) here
-                     ^~
-# ./spec/spec_helper.rb:1:in `require_relative'
-# ./spec/spec_helper.rb:1:in `<top (required)>'
-# ./spec/my_each_spec.rb:1:in `require_relative'
-# ./spec/my_each_spec.rb:1:in `<top (required)>'
-No examples found.
-
-Finished in 0.00004 seconds (files took 0.09689 seconds to load)
-0 examples, 0 failures, 1 error occurred outside of examples
