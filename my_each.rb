3aa31a0cf700157e7e10f83ea4ad4072cfb315bb
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-08 20:16:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,5 +2,24 @@ collection = []
 
 def my_each(words) do |word| # put argument(s) here
   # code here
-  puts word
+  x = 0 
+  
+  while x 
+end
+
+
+
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
