50655a1169c0ee517fe236d7c35129ca429d27ba
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-08 20:20:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,12 +5,13 @@ def my_each(words) do |word| # put argument(s) here
   if block_given?
   x = 0 
   
-  while x < words.length 
+  while x < word.length 
   yield(word[x])
   x = x + 1 
   end 
-    words
+    word
   else
    puts "Where's my block?"
   end 
 end 
+
